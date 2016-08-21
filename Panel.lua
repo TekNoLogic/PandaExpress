835cae30b16ad70fd66a132f69ca196b6e823ee6
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-08-20 21:14:51 -0600

    Scrolling!

--- Panel.lua
+++ Panel.lua
@@ -26,6 +26,7 @@ ns.panel:SetBackdropColor(TOOLTIP_DEFAULT_BACKGROUND_COLOR.r, TOOLTIP_DEFAULT_BA
 ns.panel:EnableMouse(true)
 
 
+local NUM_BUTTONS = 8
 local buttons = {}
 local butt = ns.NewCraftButton(ns.panel)
 butt:SetPoint("TOPLEFT", 10, -10)
@@ -33,24 +34,38 @@ butt:SetPoint("RIGHT", -2, 0)
 buttons[1] = butt
 
 local anchor = butt
-for i=1,7 do
+for i=2,NUM_BUTTONS do
   local butt = ns.NewCraftButton(ns.panel)
   butt:SetPoint("TOPLEFT", anchor, "BOTTOMLEFT", 0, -7)
   butt:SetPoint("RIGHT", anchor)
   anchor = butt
-  buttons[i+1] = butt
+  buttons[i] = butt
 end
 
 
 local recipe_ids
+local offset = 0
 local function Refresh()
   recipe_ids = C_TradeSkillUI.GetFilteredRecipeIDs()
   for i,butt in pairs(buttons) do
-    butt:SetRecipe(recipe_ids[i])
+    butt:SetRecipe(recipe_ids[i+offset])
   end
 end
-hooksecurefunc(TradeSkillFrame.RecipeList, "RebuildDataList", Refresh)
+hooksecurefunc(TradeSkillFrame.RecipeList, "RebuildDataList", function()
+  offset = 0
+  Refresh()
+end)
 hooksecurefunc(TradeSkillFrame.DetailsFrame, "RefreshDisplay", function()
   -- Tiny delay to allow tekReagentCost to scan all the recipes
   C_Timer.After(.01, Refresh)
+end)
+
+
+ns.panel:EnableMouseWheel(true)
+ns.panel:SetScript("OnMouseWheel", function(self, value)
+  local max = #recipe_ids - NUM_BUTTONS
+  offset = offset - value * NUM_BUTTONS / 2
+  if offset > max then offset = max end
+  if offset < 0 then offset = 0 end
+  Refresh()
 end)
