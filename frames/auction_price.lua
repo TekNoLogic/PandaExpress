4b299c6750d8ae29e4776d564491d14f2cb5155a
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-20 17:34:16 -0700

    make that actually set the text

--- frames/auction_price.lua
+++ frames/auction_price.lua
@@ -7,13 +7,11 @@ local UNKNOWN = GRAY_FONT_COLOR_CODE.. "???"
 
 local function SetValue(self, recipe_id)
   local link = ns.GetResultItemLink(recipe_id)
-  if link then
-    local ah = GetAuctionBuyout and GetAuctionBuyout(link)
-    local ah_price = ah and ns.GS(ah) or UNKNOWN
-    self.ah:SetText(ah_price)
-  else
-    self.ah:SetText()
-  end
+  if not link then return self:SetText() end
+
+  local ah = GetAuctionBuyout and GetAuctionBuyout(link)
+  local ah_price = ah and ns.GS(ah) or UNKNOWN
+  self:SetText(ah_price)
 end
 
 
